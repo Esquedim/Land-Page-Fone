8b106297a49f32cfa70fcd68af941f4d43c0d994
Malone Collioni Esquedim <esquedimmalone@gmail.com>
2025-09-09 22:14:12 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -28,7 +28,22 @@ window.addEventListener('load', function() {
     }
   });
 
-
+  const menuToggle = document.getElementById('menu-toggle');
+  const menu = document.querySelector('.conteiner-lista');
+  const icon = menuToggle.querySelector('i'); // pega o ícone dentro do botão
+  
+  menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('show');
+    
+    if (menu.classList.contains("show")) {
+      icon.classList.remove("fa-bars")
+      icon.classList.add("fa-x")
+    }else{
+      icon.classList.remove("fa-x")
+      icon.classList.add("fa-bars")
+    }
+  });
+  
   
   
 });
