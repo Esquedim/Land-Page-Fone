7e3a2caf310773453ee8e66bce676d922a78fdcb
Malone Collioni Esquedim <esquedimmalone@gmail.com>
2025-09-11 12:33:01 -0300

    mudeis os carrosseis e atualizei uns detalhes de js e css

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
 
-
-
 window.addEventListener('load', function() {
-  const swiper = new Swiper(".mySwiper", {
-    loop: true,
-    autoplay: { delay: 3000, disableOnInteraction: false },
-    pagination: { el: ".swiper-pagination", clickable: true },
-    navigation: { nextEl: ".swiper-next", prevEl: ".swiper-prev" },
-    slidesPerView: 2
-  });
-
-
-
-  const cardsSwiper = new Swiper('.cards-swiper', {
-    slidesPerView: 1,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      640: { slidesPerView: 2 },
-      1024: { slidesPerView: 3 },
-    }
-  });
 
   const menuToggle = document.getElementById('menu-toggle');
   const menu = document.querySelector('.conteiner-lista');
